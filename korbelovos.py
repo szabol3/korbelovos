06ee8804404f21242989c842f71842c37cd3eb2e
Szabo Laszlo <szabol3@idomsoft.hu>
2025-05-22 20:51:21 +0200

    Create project

---
+++ korbelovos.py
@@ -0,0 +1,98 @@
+# Soma programja
+# Ez a program egy egyszeru jatekot valosit meg, ahol a felhasznalo egy ellenseget lovoldozhet
+# A program a felhasznalo altal megadott tipusu loveset kezeli
+# A loves tipusa lehet: egyenes, atlos, homing
+
+def loves(lovestipus):
+    """
+    Ez a fuggveny az ellenseg loveset kezeli
+    """
+    if lovestipus == "egyenes":
+        # Egyenes loves
+        print("Egyenes loves!")
+        # Itt lehetne a loves logikajat megvalositani
+        # pl. a loves iranyat, sebesseget, stb.
+    elif lovestipus == "atlos":
+        # Átlós lövés
+        print("Átlós lövés!")
+        # Itt lehetne a loves logikajat megvalositani
+        # pl. a loves iranyat, sebesseget, stb.
+    elif lovestipus == "homing":
+        # Követő lövés
+        print("Követő lövés!")
+        # Itt lehetne a loves logikajat megvalositani
+        # pl. a loves iranyat, sebesseget, stb.
+    else:
+        # Alapértelmezett lövés (egyenes)
+        print("Alapértelmezett (egyenes) lövés!")
+        # Itt lehetne a loves logikajat megvalositani
+        # pl. a loves iranyat, sebesseget, stb.
+    
+    return
+
+def lovesrajz():
+    """
+    Ez a függvény rajzolja ki a lövéseket
+    """
+    # Itt lehetne a loves rajzolasa logikajat megvalositani
+    # pl. a loves iranyat, sebesseget, stb.
+    print("Lövés rajzolása...")
+    return
+
+def korrajz():
+    """
+    Ez a függvény rajzolja ki a kört
+    """
+    # Itt lehetne a kor rajzolasa logikajat megvalositani
+    # pl. a loves iranyat, sebesseget, stb.
+    print("Kör rajzolása...")
+    return
+
+def talalatfigyeles():
+    """
+    Ez a függvény kezeli a lövések talalatfigyeleseit
+    """
+    # Itt lehetne a talalatfigyeles logikajat megvalositani
+    # pl. a loves iranyat, sebesseget, stb.
+    print("Találatfigyelés...")
+    return
+
+def gombfigyeles():
+    """
+    Ez a függvény kezeli a gombnyomásokat
+    """
+    # Itt lehetne a gombfigyeles logikajat megvalositani
+    # pl. a loves iranyat, sebesseget, stb.
+    print("Gombfigyelés...")
+    return
+
+def urhajorajz():
+    """
+    Ez a függvény rajzolja ki az űrhajót
+    """
+    # Itt lehetne az urhajo rajzolasa logikajat megvalositani
+    # pl. a loves iranyat, sebesseget, stb.
+    print("Űrhajó rajzolása...")
+    return
+def ellensegrajz():
+    """
+    Ez a függvény rajzolja ki az ellenséget
+    """
+    # Itt lehetne az ellenseg rajzolasa logikajat megvalositani
+    # pl. a loves iranyat, sebesseget, stb.
+    print("Ellenség rajzolása...")
+    return
+
+def hpkijelzes():
+    """
+    Ez a függvény kezeli a HP kijelzést
+    """
+    # Itt lehetne a HP kijelzes logikajat megvalositani
+    # pl. a loves iranyat, sebesseget, stb.
+    print("HP kijelzés...")
+    return
+
+lovedekek = []  # Lista a lövések tárolására
+# tipus, irany, pozicio
+def main():
+    """
